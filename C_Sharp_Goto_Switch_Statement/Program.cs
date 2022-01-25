6d773d3780bd44346aa7f60941a3c1541c82d2c3
Creg Sullivan <cregcsullivan1024@gmail.com>
2022-01-25 13:01:33 -0600

    Add project files.

---
+++ C_Sharp_Goto_Switch_Statement/Program.cs
@@ -0,0 +1,30 @@
+﻿// C# program to illustrate the
+// use of goto in switch statement
+using System;
+
+public class GFG
+{
+
+	// Main Method
+	public static void Main(String[] args)
+	{
+		int greeting = 2;
+
+		switch (greeting)
+		{
+			case 1:
+				Console.WriteLine("Hello");
+				goto default;
+			case 2:
+				Console.WriteLine("Bonjour");
+				goto case 3;
+			case 3:
+				Console.WriteLine("Namaste");
+				goto default;
+			default:
+				Console.WriteLine("Entered value is: " + greeting);
+				break;
+		}
+	}
+}
+
